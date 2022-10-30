8a296b96050984b62f81eab15842fe750d8cd534
KaungMyatThu-101 <kaungmyatthu.dev@gmail.com>
2022-10-31 01:14:32 +0630

    First

--- part9_JSProjectALL/quoteGenerator/script.js
+++ part9_JSProjectALL/quoteGenerator/script.js
@@ -1,1 +1,19 @@
 console.log("hello project");
+let quotesData = [];
+
+const randomQuote = () => {
+  const quote = quotesData[Math.floor(Math.random() * quotesData.length)];
+  console.log(quote);
+};
+
+async function quoteGenerator() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl); //api data and it will get json format; so need to convert js format
+    quotesData = await response.json();
+    randomQuote();
+  } catch (error) {
+    console.log(error);
+  }
+}
+quoteGenerator();
