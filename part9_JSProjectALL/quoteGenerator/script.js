7c9864488e0f0b82b10adf3ac6837b8969f1fb59
KaungMyatThu-101 <kaungmyatthu.dev@gmail.com>
2022-10-31 03:12:46 +0630

    First

--- part9_JSProjectALL/quoteGenerator/script.js
+++ part9_JSProjectALL/quoteGenerator/script.js
@@ -1,12 +1,44 @@
-console.log("hello project");
+const newQuote = document.getElementById("new-quote");
+const quotesHTML = document.getElementById("quote");
+const author = document.getElementById("author");
+const quoteContainer = document.getElementById("quote-container");
+const loader = document.getElementById("loader");
+
 let quotesData = [];
 
-const randomQuote = () => {
-  const quote = quotesData[Math.floor(Math.random() * quotesData.length)];
-  console.log(quote);
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 };
 
+const dataShow = () => {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+};
+
+const randomQuote = () => {
+  loading();
+  const quote = quotesData[Math.floor(Math.random() * quotesData.length)];
+  quotesHTML.textContent = quote.text;
+  author.textContent = quote.author;
+  if (!quote.author) {
+    author.textContent = "Anonymous";
+  } else {
+    author.textContent = quote.author;
+  }
+
+  if (quote.text.length > 100) {
+    quotesHTML.classList.add("long-quote");
+  } else {
+    quotesHTML.classList.remove("long-quote");
+  }
+  dataShow();
+};
+newQuote.addEventListener("click", randomQuote);
+
 async function quoteGenerator() {
+  loading();
+  // const apiUrl = "https://type.fit/api/quotes";
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl); //api data and it will get json format; so need to convert js format
